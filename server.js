1debb1cc477ecf1911af4ac9acc14ebbe705cad7
Collin Prusse <collinprusse@mac.com>
2018-05-29 15:25:37 -0600

    correct dep paths

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
-const express = require('express');
 const path = require('path');
-const port = process.env.PORT || 8080;
+const express = require('express');
 const app = express();
+const publicPath = path.join(__dirname, '..', 'public');
+const port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname));
+app.use(express.static(publicPath));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve('public', 'index.html'));
+    res.sendFile(path.resolve(publicPath, 'index.html'));
 });
 
 app.listen(port);
